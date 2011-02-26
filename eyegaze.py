116abda4516e1326ac8f054447ee3f0783b27333
Ryan Hope <rmh3093@gmail.com>
2011-02-26 10:12:04 -0500

    start adding readloop

--- eyegaze.py
+++ eyegaze.py
@@ -1,4 +1,5 @@
 import socket, math, string
+from threading import Thread
 
 class EyeGaze(object):
     """This is a simple package for connecting with LC Technologies EGServer"""
@@ -8,21 +9,23 @@ class EyeGaze(object):
         self.host = host
         self.port = port
         
+    def _read_loop(self):
+        pass   
+        
     def connect(self):
+        """Connect to EGServer"""
         ret = None
         try:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.s.connect((self.host, self.port))
+            self.thread = Thread(target=self._read_loop)
+            self.thread.run()
         except socket.error, msg:
             ret = msg
         return ret
     
     def _sendCommand(self, msg):
-        n = 8 - len(msg) % 8
-        pad = ''
-        for a in range(0,n):
-            pad = '%s%s' % (pad, '\x00')
-        self.s.send('%s%s' % (msg, pad))
+        self.s.send(msg)
     
     def _mod(self, x, y):
         return int(x / y), int(math.fmod(x, y))
@@ -44,14 +47,23 @@ class EyeGaze(object):
         s = 8 - l % 8 + l
         return string.ljust(msg,s,'\x00')
     
+    def test_message(self, command, body=""):
+        return self._format_message(command, body)
+    
     def calibrate(self):
+        """Start calibration procedure"""
         msg = self._format_message(10)
         self._sendCommand(msg)
         
-    def start(self):
+    def data_start(self):
+        """Start data logging"""
         msg = self._format_message(30)
         self._sendCommand(msg)
         
-    def stop(self):
+    def data_stop(self):
+        """Stop data logging"""
         msg = self._format_message(31)
         self._sendCommand(msg)
+        
+    def __del__(self):
+        pass
