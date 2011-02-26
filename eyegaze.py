9b2cbfc4a6ad89a504cc103fe358a92364069643
Ryan Hope <rmh3093@gmail.com>
2011-02-26 18:08:41 -0500

    calibration calls complete (untested)

--- eyegaze.py
+++ eyegaze.py
@@ -10,6 +10,10 @@ class EyeGaze(object):
         self.port = port
         self.s = None
         
+        self.eg_color = (0,0,0)
+        self.eg_diameter = 0
+        self.eg_font = None
+        
     def _read_message(self):
         ret = None
         header = map(ord,self.s.recv(4))
@@ -31,23 +35,48 @@ class EyeGaze(object):
                 if val[0] == 0: # eg-gaze-info
                     pass
                 elif val[0] == 11: # eg-ws-query
-                    pass
+                    w,h = self.screen.get_size()
+                    body = "%04d,%04d,%04d,%04d,%04d,%04d,%04d,%04d" % (340, 272, w, h, w, h, 0, 0)                        
+                    self._send_message(self._format_message(12, body))
                 elif val[0] == 13: # eg-clear-screen
-                    pass
+                    pygame.event.post(pygame.event.Event(pygame.USEREVENT, {'type': 'clear'}))
                 elif val[0] == 14: # eg-set-color
-                    pass
+                    self.eg_color = (ord(val[1][2]),ord(val[1][1]),ord(val[1][0]))
                 elif val[0] == 15: # eg-set-diameter
-                    pass
+                    self.eg_diameter = (ord(val[1][0]) * 256) + ord(val[1][1])
                 elif val[0] == 16: # eg-draw-circle
-                    pass
+                    code = {
+                            'type': 'circle',
+                            'x': (ord(val[1][0]) * 256) + ord(val[1][1]),
+                            'y': (ord(val[1][2]) * 256) + ord(val[1][3]),
+                            'diameter': self.eg_diameter,
+                            'color': self.eg_color
+                            }
+                    pygame.event.post(pygame.event.Event(pygame.USEREVENT, code))
                 elif val[0] == 17: # eg-draw-cross
-                    pass
+                    code = {
+                            'type': 'cross',
+                            'x': (ord(val[1][0]) * 256) + ord(val[1][1]),
+                            'y': (ord(val[1][2]) * 256) + ord(val[1][3]),
+                            'diameter': self.eg_diameter,
+                            'color': self.eg_color
+                            }
+                    pygame.event.post(pygame.event.Event(pygame.USEREVENT, code))
                 elif val[0] == 18: # eg-draw-text
-                    pass
+                    code = {
+                            'type': 'text',
+                            'x': (ord(val[1][0]) * 256) + ord(val[1][1]),
+                            'y': (ord(val[1][2]) * 256) + ord(val[1][3]),
+                            'diameter': self.eg_diameter,
+                            'color': self.eg_color,
+                            'text': val[1][4:]
+                            }
+                    pygame.event.post(pygame.event.Event(pygame.USEREVENT, code))
                 elif val[0] == 19: # eg-cal-complete
-                    pass
+                    pygame.event.post(pygame.event.Event(pygame.USEREVENT, {'type': 'complete'}))
                 elif val[0] == 20: # eg-cal-aborted
-                    pass
+                    pygame.event.clear(pygame.USEREVENT)
+                    pygame.event.post(pygame.event.Event(pygame.USEREVENT, {'type': 'abort'}))
         
     def _send_message(self, msg):
         self.s.send(msg)
@@ -91,9 +120,52 @@ class EyeGaze(object):
         self.s.close()
         self.s = None
     
-    def calibrate(self):
+    def calibrate(self, screen=pygame.display.get_surface()):
         """Start calibration procedure"""
+        if screen:
+            self.screen = screen
+        else:
+            pygame.display.init()
+            pygame.font.init()
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN|pygame.HWSURFACE|pygame.DOUBLEBUF)
+            self.eg_font = pygame.font.Font(None, 14)
         self._send_message(self._format_message(10))
+        ret = True
+        cont = True
+        while cont:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        ret = False
+                        cont = False
+                elif event.type == pygame.USEREVENT:
+                    if event.code.type == 'clear':
+                        self.screen.fill((0,0,0))
+                        pygame.display.flip()
+                    elif event.code.type == 'circle':
+                        pygame.draw.circle(self.screen, event.code.color, 
+                                           (event.code.x,event.code.y),
+                                           event.code.diameter/2)
+                        pygame.display.flip()
+                    elif event.code.type == 'cross':
+                        pygame.draw.line(self.screen, event.code.color,
+                                         ((event.code.x-event.code.diameter/2), event.code.y),
+                                         ((event.code.x+event.code.diameter/2), event.code.y))
+                        pygame.draw.line(self.screen, event.code.color,
+                                         (event.code.x, (event.code.y-event.code.diameter/2)),
+                                         (event.code.x, (event.code.y+event.code.diameter/2)))
+                        pygame.display.flip()
+                    elif event.code.type == 'text':
+                        text = self.eg_font.render(event.code.text, True, event.code.color)
+                        text_rect = text.get_rect()
+                        text_rect.center = (event.code.x, event.code.y)
+                        pygame.display.flip()
+                    elif event.code.type == 'complete':
+                        cont = False
+                    elif event.code.type == 'abort':
+                        ret = False
+                        cont = False
+        return ret
         
     def data_start(self):
         """Start data logging"""
