7f9e7007bc934818a7e91cc1f7977aaea519ed95
Ryan Hope <rmh3093@gmail.com>
2011-03-06 17:43:08 -0500

    add some documentation

--- nback.py
+++ nback.py
@@ -2,13 +2,19 @@ import time, random
 from collections import deque
 
 class NBack(object):
+    """N-Back Task"""
     
     NUMERIC = 0
     ALPHA1 = 1
     ALPHA2 = 2
     ALPHA3 = 3
 
-    def __init__(self, p=None, n=1, type=ALPHA1):
+    def __init__(self, n=1, p=None, type=ALPHA1):
+        """Initialize an NBack object; takes 3 optional arguments:
+        n       The set size of stimlui to remember
+        p       The probability that the n-th back will match
+        type    The stimuli pool to use"""
+        
         super(NBack, self).__init__()
         
         self.p = p
@@ -24,8 +30,9 @@ class NBack(object):
             pool = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         
         elif type == self.ALPHA1:
-            pool = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            pool = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
             
         elif type == self.ALPHA2:
             pool = ['b', 'c', 'd', 'g', 'j', 'k', 'l', 'm',
@@ -38,6 +45,8 @@ class NBack(object):
         return pool
     
     def next(self):
+        """Returns the next stimlui"""
+        
         new = random.choice(self.pool)
         if len(self.buffer) == self.n + 1:
             while new == self.buffer[-2]:
@@ -55,6 +64,10 @@ class NBack(object):
         return self.buffer[0]
     
     def respond(self, ans):
+        """Pass the ans argument to indicate if n-th back was a match.
+        Returns None if there are not yet n+1 items in the buffer else it
+        returns the correctness of ans."""
+        
         ret = None
         if len(self.buffer) == self.n + 1:
             match = self.buffer[0] == self.buffer[-1]
