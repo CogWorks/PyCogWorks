6158269380f4218126418826cf3144596b539559
Ryan Hope <rmh3093@gmail.com>
2011-04-17 13:27:37 -0400

    read netstation simple binary file

--- netstation/spectral.py
+++ netstation/spectral.py
@@ -15,38 +15,46 @@ if __name__ == '__main__':
  
     for arg in sys.argv[1:]:
         f = open(arg,'r')
-        header, events, data, evtData = simpleBinary.read(f)
+        header, data, events = simpleBinary.read(f)
         f.close()
-        print header, events
+        print header
+        print
+        print 'nEEGData: %d' % (len(data))
+        print 'nEventData: %d' % (len(events))
+                
+        #plt.plot(events[1])
+        #plt.show()
         
         fig = plt.figure(1)
         fig.subplots_adjust(hspace=.5,wspace=.3)
-        
-        for e in evtData:
-            plt.plot(e)
-            plt.show()
-        
-        overlap = 0.5
-        window = 2 * header['smplFreq']
-        taper = np.hanning(window)
-        offset = 0
 
-        for i in range(0,header['nChan']):
-            #m = np.mean(data[i])
-            #data[i] = map(lambda x: x / m - 1, data[i])
+        overlap = 0.5
+        window = 2 * header.smplFreq
+        taper = np.hanning(window)
+
+        mod = header.nChan % 2
+        for i in range(0,header.nChan-mod):
+            power = []
             fgrid = []
-            fourier = []
-            while offset + window <= header['nSmpl']:
-                #sp = periodogram(np.multiply(data[i][offset:offset+window], taper), fs=header['smplFreq'])
-                sp = arspec(np.multiply(data[i][offset:offset+window], taper), 5, fs=header['smplFreq'])
-                fourier.append(sp[0])
-                fgrid = sp[1]
-                offset += int(window * overlap)           
+            for game in np.nonzero(events[1])[0]:
+                offset = game
+                max = offset + 300 * header.smplFreq
+                tmp = []
+                while offset + window <= max:
+                    #sp = arspec(np.multiply(data[i][offset:offset+window], taper), 12, fs=header.smplFreq)
+                    sp = periodogram(np.multiply(data[i][offset:offset+window], taper), fs=header.smplFreq)
+                    tmp.append(sp[0])
+                    fgrid = sp[1]
+                    offset += int(window * overlap)
+                print len(tmp)
+                power.append(np.mean(tmp,0))
+            print len(power)
             plt.subplot2grid((8,7),(GSN32[i][0]-1,GSN32[i][1]-1))
-            plt.plot(fgrid, np.mean(fourier,0))
-            plt.xlim([3,15])
+            plt.plot(fgrid, np.mean(power,0))
             plt.title(str(i+1))
-            offset = 0            
+            plt.axis('tight')
+            plt.xlim([45,75])
+            #plt.xlim([3,15])
+            plt.ylim([0,32])
         plt.show()
-        
         
