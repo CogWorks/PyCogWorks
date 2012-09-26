0411d088f634ba1181c0bfc6242d4ee411d94750
Ryan Hope <rmh3093@gmail.com>
2012-09-26 11:19:18 -0400

    Platform dependent time function
    Get the most accurate timestamp for the given platform

--- pycogworks/util.py
+++ pycogworks/util.py
@@ -1,6 +1,11 @@
 from Crypto.Cipher import AES
+import time, platform
 
 def rin2id(rin):
     rin = '%s%s' % (rin,rin[:7])
     cipher = AES.new(rin, AES.MODE_CBC, "0000000000000000")
     return ''.join(["%02x" % ord(x) for x in cipher.encrypt(rin)]).strip()
+
+get_time = time.time
+if platform.system() == 'Windows':
+    get_time = time.clock
